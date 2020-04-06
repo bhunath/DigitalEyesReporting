7a7625aea04f417bab09ad7326af72beb9532d5f
bhunath <bhupendra191987@gmail.com>
2020-04-06 20:47:29 +0530

    adding reporting.

--- public/reporting.js
+++ public/reporting.js
@@ -45,6 +45,7 @@ function hideyChart(){
   myChart = document.getElementById('myChart');
   myChart.style.display='NONE';
   reportContainer.style.display='NONE';
+  console.log(reportContainer);
 }
 
 function showChart(){
@@ -92,4 +93,52 @@ function displayBlinkReportPerMinuteChart(label,datasets){
       options: optionBar
   });
   console.log("charting is done");
+}
+
+function showHideBlinkReport(){
+  var reportButton = document.getElementById('reportButton');
+  console.log(reportButton);
+  var reportButtonLabel = reportButton.innerHTML;
+  if('Show Blink Report' == reportButtonLabel){
+    reportButton.innerHTML = 'Hide Blink Report';
+    // Creating a XHR object
+    let xhr = new XMLHttpRequest();
+    let url = "http://localhost:8080/fetch_blink_per_minute_data";
+    // open a connection
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          showChart();
+          var dataSets = [];
+          var backgroundColor_open = ['rgba(255, 99, 132, 0.2)','rgba(54, 162, 235, 0.2)'];
+          var borderColor = ['rgba(255, 99, 132, 1)','rgba(54, 162, 235, 1)'];
+          var label = [];
+          var blinkStatePerMinute = JSON.parse(this.responseText);
+          console.log(blinkStatePerMinute);
+          var blinkReportPerMinute = [];
+          for (var attribute in blinkStatePerMinute) {
+            label.push(attribute);
+            blinkReportPerMinute.push(blinkStatePerMinute[attribute])
+          }
+          var dataset = {
+            label: 'Blink Count',
+            fill:false,
+            backgroundColor: "rgba(225,0,0,0.4)",
+            borderColor: "rgba(225,0,0,0.4)",
+            data: blinkReportPerMinute
+          };
+          console.log(blinkStatePerMinute);
+
+          dataSets.push(dataset);
+          displayBlinkReportPerMinuteChart(label,dataSets);
+          return this.responseText;
+        }
+      };
+    xhr.send();
+
+  }else if('Hide Blink Report' == reportButtonLabel){
+    console.log('Stop is Called',take_snapshot_interval)
+    reportButton.innerHTML = 'Show Blink Report';
+    hideyChart();
+  }
 }
